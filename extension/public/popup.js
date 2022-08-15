8d5ff115c346ed870e056fa2625984db6d9a6cac
atan958 <atan958@aucklanduni.ac.nz>
2022-08-15 17:02:04 +1200

    feat: added javascript to the keywords section to fetch and display hard-coded keywords data

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -1,4 +1,4 @@
-/***** Sites *****/ 
+/********** Enabled Sites **********/ 
 
 // UI references
 const toggleList = document.getElementById("toggleList");
@@ -24,3 +24,82 @@ const globalSwitchHandler = () => {
 }
 
 globalSwitch.onclick = globalSwitchHandler;
+
+
+/********** Keywords **********/ 
+
+// keywords data
+const allowedWordsData = [
+    "ukraine",
+    "blm",
+    "gas prices"
+];
+
+const blockedWordsData = [
+    "trump",
+    "terrorist",
+    "communism",
+    "racism",
+    "isis",
+    "pizza"
+];
+
+// UI references
+const allowedBtn = document.getElementById("allowedBtn");
+const blockedBtn = document.getElementById("blockedBtn");
+const tagList = document.getElementById("tagList");
+const manageTagListBtn = document.getElementById("manageTagListBtn");
+
+// Helper: 
+const renderAllowedWords = () => {
+    const max = 4;
+    const numTagItems = allowedWordsData.length > max ? max : allowedWordsData.length;
+    const numMoreResults=  allowedWordsData.length - numTagItems;
+
+    let tagItems = "";
+    const moreResults = numMoreResults > 0 ? `<div class=\"tag-results-item\">${numMoreResults} more</div>` : "";
+
+    for (let i=0; i<numTagItems; i++) {
+        tagItems += `<div class=\"tag-item\">${allowedWordsData[i]}</div>`;
+    }
+    tagList.innerHTML = tagItems + moreResults;
+}
+
+// Helper: 
+const renderBlockedWords = () => {
+    const max = 4;
+    const numTagItems = blockedWordsData.length > max ? max : blockedWordsData.length
+    const numMoreResults=  blockedWordsData.length - numTagItems;
+
+    let tagItems = "";
+    const moreResults = numMoreResults > 0 ? `<div class=\"tag-results-item\">${numMoreResults} more</div>` : "";
+    
+    for (let i=0; i<numTagItems; i++) {
+        tagItems += `<div class=\"tag-item\">${blockedWordsData[i]}</div>`;
+    }
+    tagList.innerHTML = tagItems + moreResults;
+}
+
+// Method: 
+const allowedBtnHandler = () => {
+    manageTagListBtn.innerHTML = "Edit Allowed"
+    allowedBtn.style.backgroundColor = "#2196F3"
+    blockedBtn.style.backgroundColor = "#999999";
+    renderAllowedWords();
+}
+
+// Method: 
+const blockedBtnHandler = () => {
+    manageTagListBtn.innerHTML = "Edit Blocked"
+    allowedBtn.style.backgroundColor = "#999999"
+    blockedBtn.style.backgroundColor = "#2196F3";
+    renderBlockedWords();
+}
+
+// assign buttons their respective functions
+allowedBtn.onclick = allowedBtnHandler;
+blockedBtn.onclick = blockedBtnHandler;
+
+
+allowedBtnHandler();
+
