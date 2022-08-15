8fdd3cbdd4aa7d1bb0841592b4d7c71f5c76a05f
atan958 <atan958@aucklanduni.ac.nz>
2022-08-15 16:57:42 +1200

    feat: added javascript to the enabled sites section of the extension ui

--- extension/public/popup.js
+++ extension/public/popup.js
@@ -1,1 +1,26 @@
-console.log('Hello World!');
+/***** Sites *****/ 
+
+// UI references
+const toggleList = document.getElementById("toggleList");
+const redditSwitch = document.getElementById("redditSwitch");
+const imgurSwitch = document.getElementById("imgurSwitch");
+const twitterSwitch = document.getElementById("twitterSwitch");
+const addOtherSites = document.getElementById("addOtherSites");
+const globalSwitch = document.getElementById("globalSwitch");
+
+const globalSwitchHandler = () => {
+    if(globalSwitch.checked == true) {
+        redditSwitch.checked = false;
+        imgurSwitch.checked = false;
+        twitterSwitch.checked = false;
+
+        toggleList.style.pointerEvents = "none";
+        toggleList.style.opacity = "50%";
+    }
+    else {
+        toggleList.style.pointerEvents = "auto";
+        toggleList.style.opacity = "100%";
+    }
+}
+
+globalSwitch.onclick = globalSwitchHandler;
