a34d46cae2b723647521eecde8fa63813cf1dd80
Leonard Min <hmin697@aucklanduni.ac.nz>
2022-08-20 10:25:34 +1200

    feat-checks for new visible posts when scrolled

--- extension/public/reddit.js
+++ extension/public/reddit.js
@@ -2,53 +2,33 @@ var Reddit = {
   // CSS selector for Reddit links
   linkSelector: ".scrollerItem[id]",
 
+  // CSS selector for hidden links
+  hiddenLinkSelector: ".scrollerItem[id]:hidden",
+
+  // CSS selector for visible links
+  visibleLinkSelector: ".scrollerItem[id]:visible",
+
   // CSS selector for the Reddit posts list
   listSelector: ".rpBJOHq2PR60pnwJlUyP0",
 
-  // HTML for hide button
-  hideLinkHTML: function () {
-    return '<div class="reddit-actions"><div class="reddit-action hide">Hide</div></div>';
-  },
+  counter: 0,
+  refreshFrequency: 100,
 
-  // HTML for dashboard
-  dashboardHTML: function () {
-    var h = '<div class="reddit-dashboard">';
-    h += '<div class="reddit-page-status">';
-    h +=
-      '<div class="reddit-status total_hidden"><span class="variable"></span>Total hidden</div>';
-    h += "</div>";
-    h += '<div class="reddit-controls">';
-    h += '<div class="reddit-control hide_links">Load Hide Buttons</div>';
-    h += '<div class="reddit-control unhide_links">Unhide all</div>';
-    h += "</div>";
-    h += "</div>";
-
-    return h;
-  },
-
-  // initalize UI
+  // initalize
   initialize: function () {
-    this.setupUI();
+    this.blurImage();
+    this.checkForUpdate();
   },
 
-  // initalize all UI elements and onclick binding
-  setupUI: function () {
-    list = document.querySelector(this.listSelector);
-    list.innerHTML = this.dashboardHTML() + list.innerHTML;
-
-    document
-      .querySelector(".reddit-control.unhide_links")
-      .addEventListener("click", () => {
-        this.restoreDOM();
-        console.log("unhide all");
-      });
-
-    document
-      .querySelector(".reddit-control.hide_links")
-      .addEventListener("click", () => {
-        this.setupPostUI();
-        console.log("all links hidden");
-      });
+  checkForUpdate: function(){
+    document.addEventListener('scroll', (ev) => {
+      this.counter++;
+      console.log(this.counter);
+      if(this.counter == this.refreshFrequency){
+        this.counter = 0;
+        this.blurImage();
+      }
+    });
   },
 
   setupPostUI: function () {
